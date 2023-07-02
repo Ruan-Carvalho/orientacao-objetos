a050aa9212b75326f03c4163c85e6466b6664d09
Ruan-Carvalho <ruansobreira11@gmail.com>
2023-07-02 16:51:24 -0300

    .
    .

---
+++ Trabalho/doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"view","c":"Menu_inicial","l":"actionPerformed(ActionEvent)","u":"actionPerformed(java.awt.event.ActionEvent)"},{"p":"view","c":"Menu_inicial","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"view","c":"Menu_inicial","l":"Menu_inicial()","u":"%3Cinit%3E()"}];updateSearchResults();
